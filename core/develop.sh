bc298852384d7df0eb4ab1d34346ab93538daaae
cmuto09 <cmuto09@github.com>
2022-11-03 22:52:46 +0000

    added a start script

--- core/develop.sh
+++ core/develop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo -e "***** Bootstrapping your developer environment; running develop.sh *****"
-## mvn clean package -DskipTests -P build-web-console
+mvn clean package -DskipTests -P build-web-console
 
 BUILD_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo BUILD_VERSION = $BUILD_VERSION
